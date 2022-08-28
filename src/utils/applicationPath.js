deffa3104b736c66d6ab968df0af8e7a2aa71518
farnazii <ffarzipour92@gmail.com>
2022-08-28 09:56:48 +0430

    add relative path

--- src/utils/applicationPath.js
+++ src/utils/applicationPath.js
@@ -3,6 +3,14 @@ const PATH = {
 		return require('@/assets/img' + '/' + path);
 		// usage in components : this.$Path.GET_IMAGE_PATH('apple-icon.png')
 	},
+	RELATIVE_PATH: {
+		GET: {
+			GET_EXCHANGE: '/api/v1/latest_exchange_rate/',
+		},
+		POST: {
+			PHONE_NUMBER_OTP_REQ: '/api/v1/phone_number//',
+		},
+	},
 };
 
 export default PATH;
