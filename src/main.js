80b99af0cbb842a5f6f0876586c454e8b2c94c16
Farnazii <ffarzipour92@gmail.com>
2022-08-29 15:46:21 +0430

    add apiservice layer to vue prototype

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import PATH from '@/utils/applicationPath';
 import store from './store';
 import GlobalComponents from '@/utils/globalComponents';
 import '@/utils/globalLibraries';
+import ApiServiceLayer from './plugins/apiServiceLayer';
 
 Vue.config.productionTip = false;
 Vue.prototype.$PATH = PATH;
+Vue.prototype.$ApiServiceLayer = new ApiServiceLayer();
 Vue.use(GlobalComponents);
 
 new Vue({
