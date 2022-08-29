b576a78f68bd668e2af7a4322fa4960186342b0b
farnazii <ffarzipour92@gmail.com>
2022-08-29 09:52:51 +0430

    add code sample

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,16 @@
 <template>
 	<p>salam</p>
 </template>
+
+<script>
+export default {
+	methods: {
+		async test() {
+			const res = await this.$ApiServiceLayer.post(
+				this.$PATH.RELATIVE_PATH.POST.PHONE_NUMBER_OTP_REQ,
+				{ phone_number: '+98' + this.userPhoneNumber.substring(1) },
+			);
+		}
+	}
+}
+</script>
