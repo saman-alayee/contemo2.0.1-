7265d237e18514883525c449c7cf57fc2c2caf8e
ali-mohsenpour <alimpp766@gamil.com>
2022-08-30 00:38:14 -0700

    Create Topbar

---
+++ src/components/wallet.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="wallet app-rtl">
+        <img class="mx-1" src="@/assets/vectors/plus.svg">
+        <span class="text-white py-1">اعتبار حساب</span>
+        <span class="text-white py-1 pl-5"><b>2,325,000</b></span>
+        <span class="text-white py-1 pl-3"> تومان</span>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_shared.scss';
+.wallet {
+	display: flex;
+	width: 380px;
+	background: $color-warning;
+	border-radius: 15px;
+}
+</style>
--- src/layout/Topbar.vue
+++ src/layout/Topbar.vue
@@ -1,15 +1,26 @@
 <template>
-	<div class="topbar-container">
-		<p>inja code top bar bza</p>
+	<div class="topbar">
+		<div class="row">
+			<div class="col-lg-6">
+				<h3 class="pt-4 px-3"> سلام وقت بخیر محسن هست :)</h3>
+			</div>
+			<div class="col-lg-6">
+				<wallet class="mt-3" />
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import wallet from '@/components/wallet.vue';
 export default {
-
+	name: 'topbar',
+	components: { wallet },
 };
 </script>
 
 <style lang="scss" scoped>
-
+.topbar {
+	width: 100%;
+}
 </style>
