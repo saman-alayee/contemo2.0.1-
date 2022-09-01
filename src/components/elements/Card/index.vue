e5c90201d90fe9ff35d85a55687034230b17d705
farnazii <ffarzipour92@gmail.com>
2022-09-01 12:27:12 +0430

    change card slot

--- src/components/elements/Card/index.vue
+++ src/components/elements/Card/index.vue
@@ -1,41 +1,8 @@
 <template>
-	<div class="base-card-container" :style="{ background, width, color }">
-		<div class="card-body">
-			<h5 class="card-title-component" :style="{ color }">{{ title }}</h5>
-			<p class="card-text-component">
-				{{ description }}
-			</p>
-			<div class="card-footer-component">{{ footer }}</div>
-		</div>
+	<div class="base-card-container">
+		<slot name="content"></slot>
 	</div>
 </template>
-<script>
-export default {
-	props: {
-		title: {
-			type: String,
-		},
-		description: {
-			type: String,
-		},
-		background: {
-			type: String,
-			default: 'white',
-		},
-		width: {
-			type: String,
-			default: '100%',
-		},
-		footer: {
-			type: String,
-		},
-		color: {
-			type: String,
-			default: 'black',
-		},
-	},
-};
-</script>
 
 <style lang="scss" scoped>
 .base-card-container {
@@ -44,20 +11,5 @@ export default {
 	background-color: #fff;
 	box-shadow: -2px 4px 10px rgba(0, 0, 0, 0.05);
 	border-radius: 20px;
-}
-.card-title-component {
-	font-style: normal;
-	font-weight: 600;
-	font-size: 20px;
-	line-height: 20px;
-}
-.card-text-component {
-	font-style: normal;
-	font-weight: 200;
-	font-size: 12px;
-	line-height: 20px;
-}
-.card-footer-component {
-	width: 100%;
 }
 </style>
