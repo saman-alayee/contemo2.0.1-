842c317160840ce154df7f29947c3c0c48c744bc
Mahshid-Rs <mahshid.rasoulii@gmail.com>
2022-09-01 11:52:40 +0430

    Fix wallet size

--- src/components/wallet.vue
+++ src/components/wallet.vue
@@ -1,10 +1,14 @@
 <template>
 	<div class="wallet app-rtl">
-        <img class="mx-1" src="@/assets/vectors/plus.svg">
-        <span class="text-white py-1">اعتبار حساب</span>
-        <span class="text-white py-1 pl-5"><b>2,325,000</b></span>
-        <span class="text-white py-1 pl-3"> تومان</span>
-    </div>
+		<div class="account-credit">
+			<img class="mx-1" src="@/assets/vectors/plus.svg" />
+			<span class="text-white">اعتبار حساب</span>
+		</div>
+		<div class="amount-box">
+			<span class="amount "><b>2,325,000</b></span>
+			<span class="currency"> تومان</span>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -15,8 +19,28 @@ export default {};
 @import '@/assets/scss/_shared.scss';
 .wallet {
 	display: flex;
-	width: 380px;
+	width: 100%;
 	background: $color-warning;
 	border-radius: 15px;
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 0;
+}
+.account-credit {
+	display: flex;
+	justify-content: flex-start;
+}
+.amount-box {
+	display: flex;
+	justify-content: flex-end;
+	padding-right: 56px;
+}
+.amount {
+	color: $color-white;
+}
+.currency {
+	padding-right: 16px;
+    padding-left: 24px;
+	color: $color-white;
 }
 </style>
