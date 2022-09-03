3c8062ddc58bf539a84e3f0dc405ecc1711afbbd
saman1945 <saman.alaii10@gmail.com>
2022-09-03 16:40:57 +0430

    gray_account

---
+++ src/components/elements/Card/socialCard.vue
@@ -0,0 +1,89 @@
+<template>
+	<div class="partner-card-container">
+		<BaseCard class="base-color">
+			<template slot="content">
+				<div class="partner-item-container">
+					<div class="row">
+						<div class="col-md-5">
+							<img src="@/assets/img/icon/instagramIcon.svg" alt="" />
+						</div>
+
+						<div class="col-md-7 text-container">
+							<div class="textFa-container">
+								<p>{{ textFa }}</p>
+							</div>
+							<div class="textEn-container">
+								<p>{{ textEN }}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</template>
+		</BaseCard>
+	</div>
+</template>
+
+<script>
+import BaseCard from '@/components/elements/Card/index.vue';
+export default {
+	components: {
+		BaseCard,
+	},
+	props: {
+		textFa: {
+			type: String,
+			required: true,
+		},
+		textEN: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_shared.scss';
+
+.partner-card-container {
+	width: 210px;
+	height: 88px;
+}
+.base-color {
+	border: 1px solid #a5a5a5;
+	border-radius: 20px;
+	background-color: #f5f5f5;
+}
+.partner-item-container {
+}
+.name-container {
+	p {
+		font-weight: 600;
+		font-size: 20px;
+		line-height: 23px;
+		text-align: right;
+		color: #ffffff;
+	}
+}
+.text-container{
+    margin-top: 8px;
+}
+.textFa-container {
+	p {
+		font-style: normal;
+		font-weight: 500;
+		font-size: 20px;
+		line-height: 23px;
+		color: #000000;
+	}
+}
+.textEn-container {
+	p {
+		font-style: normal;
+		font-weight: 300;
+		font-size: 12px;
+		line-height: 20px;
+		color: #a5a5a5;
+	}
+}
+</style>
--- src/components/pageComponents/Dashboard/index.vue
+++ src/components/pageComponents/Dashboard/index.vue
@@ -1,128 +1,180 @@
 <template>
-	<div class="dashboard-component">
-		<div class="row">
-			<div class="col-lg-4">
-				<taskCard
-					:title="item.title"
-					:description="item.description"
-					:avatar="item.avatars"
-					v-for="item in taskDatas"
-					:key="item.id"
-					class="mt-1"
-				/>
-			</div>
+  <div class="dashboard-component">
+    <div class="row">
+      <div class="col-lg-4">
+        <taskCard
+          :title="item.title"
+          :description="item.description"
+          :avatar="item.avatars"
+          v-for="item in taskDatas"
+          :key="item.id"
+          class="mt-1"
+        />
+      </div>
 
-			<div class="col-lg-8">
-				<div class="d-flex justify-content-end"><BlueCard offline="154" online="54" project="253" /></div>
-				<LineChart :width="900" :height="400" :chart-data="chartData" class="mt-4" />
-				<h4 class="pt-1">همکاران</h4>
-				<div class="d-flex">
-					<PartnerCard
-						v-for="item in partnerData"
-						:key="item.id"
-						:role="item.role"
-						:name="item.name"
-						:avatar="item.avatar"
-						class="mx-1 mt-1"
-					/>
-				</div>
-			</div>
-		</div>
-	</div>
+      <div class="col-lg-8">
+        <div class="d-flex justify-content-end">
+          <BlueCard offline="154" online="54" project="253" />
+        </div>
+        <LineChart :width="900" :height="400" :chart-data="chartData" class="mt-4" />
+        <h4 class="pt-1">همکاران</h4>
+        <div class="d-flex">
+          <PartnerCard
+            v-for="item in partnerData"
+            :key="item.id"
+            :role="item.role"
+            :name="item.name"
+            :avatar="item.avatar"
+            class="mx-1 mt-1"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import PartnerCard from '@/components/elements/Card/partnerCard.vue';
-import taskCard from '@/components/elements/Card/taskCard.vue';
-import LineChart from '@/components/charts/lineChart.vue';
-import BlueCard from '../../elements/Card/blueCard.vue';
+import PartnerCard from "@/components/elements/Card/partnerCard.vue";
+import taskCard from "@/components/elements/Card/taskCard.vue";
+import LineChart from "@/components/charts/lineChart.vue";
+import BlueCard from "../../elements/Card/blueCard.vue";
 export default {
-	components: {
-		PartnerCard,
-		taskCard,
-		LineChart,
-		BlueCard,
-	},
-	data() {
-		return {
-			partnerData: [
-				{ name: 'سامان علایی', role: 'گرافیست', avatar: '' },
-				{ name: 'سامان علایی', role: 'گرافیست', avatar: '' },
-				{ name: 'سامان علایی', role: 'گرافیست', avatar: '' },
-			],
-			taskDatas: [
-				{
-					title: 'عنوان تسک',
-					description: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با',
-					avatars: 'vectors/avatar.svg',
-				},
-				{
-					title: 'عنوان تسک',
-					description: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با',
-					avatars: 'vectors/avatar.svg',
-				},
-				{
-					title: 'عنوان تسک',
-					description: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با',
-					avatars: 'vectors/avatar.svg',
-				},
-			],
-			chartData: {
-				labels: [
-					'00',
-					'01',
-					'02',
-					'03',
-					'04',
-					'05',
-					'06',
-					'07',
-					'08',
-					'09',
-					'10',
-					'11',
-					'12',
-					'13',
-					'14',
-					'15',
-					'16',
-					'17',
-					'18',
-					'19',
-					'20',
-					'21',
-					'22',
-					'23',
-					'24',
-					'25',
-					'26',
-					'27',
-					'28',
-					'29',
-					'30',
-				],
-				datasets: [
-					{
-						label: 'تعداد لایک',
-						backgroundColor: '#1212c4',
-						color: '#1212c4',
-						data: [
-							40, 39, 70, 40, 39, 80, 70, 49, 10, 55, 49, 43, 39, 20, 40, 39, 80, 39, 10, 40, 39,
-							40, 39, 10, 40, 39, 80,
-						],
-					},
-					{
-						label: 'تعداد فالور',
-						backgroundColor: '#b4c412',
-						data: [
-							10, 29, 40, 44, 39, 80, 75, 42, 10, 33, 49, 22, 39, 30, 88, 39, 80, 39, 10, 40, 29,
-							20, 19, 10, 40, 39, 80,
-						],
-					},
-				],
-			},
-		};
-	},
+  components: {
+    PartnerCard,
+    taskCard,
+    LineChart,
+    BlueCard,
+  },
+  data() {
+    return {
+      partnerData: [
+        { name: "سامان علایی", role: "گرافیست", avatar: "" },
+        { name: "سامان علایی", role: "گرافیست", avatar: "" },
+        { name: "سامان علایی", role: "گرافیست", avatar: "" },
+      ],
+      taskDatas: [
+        {
+          title: "عنوان تسک",
+          description: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با",
+          avatars: "vectors/avatar.svg",
+        },
+        {
+          title: "عنوان تسک",
+          description: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با",
+          avatars: "vectors/avatar.svg",
+        },
+        {
+          title: "عنوان تسک",
+          description: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با",
+          avatars: "vectors/avatar.svg",
+        },
+      ],
+      chartData: {
+        labels: [
+          "00",
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "27",
+          "28",
+          "29",
+          "30",
+        ],
+        datasets: [
+          {
+            label: "تعداد لایک",
+            backgroundColor: "#1212c4",
+            color: "#1212c4",
+            data: [
+              40,
+              39,
+              70,
+              40,
+              39,
+              80,
+              70,
+              49,
+              10,
+              55,
+              49,
+              43,
+              39,
+              20,
+              40,
+              39,
+              80,
+              39,
+              10,
+              40,
+              39,
+              40,
+              39,
+              10,
+              40,
+              39,
+              80,
+            ],
+          },
+          {
+            label: "تعداد فالور",
+            backgroundColor: "#b4c412",
+            data: [
+              10,
+              29,
+              40,
+              44,
+              39,
+              80,
+              75,
+              42,
+              10,
+              33,
+              49,
+              22,
+              39,
+              30,
+              88,
+              39,
+              80,
+              39,
+              10,
+              40,
+              29,
+              20,
+              19,
+              10,
+              40,
+              39,
+              80,
+            ],
+          },
+        ],
+      },
+    };
+  },
 };
 </script>
 
