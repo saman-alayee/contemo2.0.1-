826e2273a1cc7cfec8f0feac9cc8016c6fc1414c
saman1945 <saman.alaii10@gmail.com>
2022-09-03 12:57:32 +0430

    chip_finall

---
+++ src/components/elements/Card/chipCard.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="base-card-container">
+		<div class="row">
+			<div class="col-md-7">
+				<p>{{ title }}</p>
+			</div>
+			<div class="col-md-5">
+				<div class="img-container">
+					<img src="@/assets/vectors/trash.svg" alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	components: {},
+	props: {
+		title: {
+			type: String,
+			// required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_shared.scss';
+
+.base-card-container {
+	padding: 8px;
+	height: 42px;
+	border: 1px solid $color-primary;
+	filter: drop-shadow(-2px 4px 10px rgba(0, 0, 0, 0.05));
+	border-radius: 10px;
+	white-space: nowrap;
+	text-align: center;
+}
+.base-card-container {
+	p {
+		font-style: normal;
+		font-weight: 300;
+		font-size: 15px;
+		line-height: 20px;
+		color: $color-primary;
+		margin-top: 3px;
+	}
+}
+</style>
+
