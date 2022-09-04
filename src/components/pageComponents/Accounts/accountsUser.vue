b496a91e0ae46f3f58ea79239e8436bb898d5d05
ali-mohsenpour <alimpp766@gamil.com>
2022-09-04 01:39:40 -0700

    Make Account Route

---
+++ src/components/pageComponents/Accounts/accountsUser.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="accounts-user">
+		<h1>Hello Accounts User</h1>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/accounts/index.vue
+++ src/views/accounts/index.vue
@@ -1,3 +1,12 @@
 <template>
-	<p>safe list account ha </p>
+	<accountsUser />
 </template>
+
+<script>
+import accountsUser from '../../components/pageComponents/Accounts/accountsUser.vue';
+export default {
+	components: { accountsUser },
+};
+</script>
+
+<style></style>
