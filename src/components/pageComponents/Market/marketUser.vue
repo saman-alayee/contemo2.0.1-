ee302f9ab953f4d75c7c31c313300d2f5a39f258
Mahshid-Rs <mahshid.rasoulii@gmail.com>
2022-09-06 11:05:55 +0430

    Add see all part

--- src/components/pageComponents/Market/marketUser.vue
+++ src/components/pageComponents/Market/marketUser.vue
@@ -13,7 +13,13 @@
 				</div>
 			</div>
 		</div>
-		<span class="content-heading">تصاویر</span>
+		<div class="heading-container row">
+			<span class="content-heading col-10">تصاویر</span>
+			<div class="see-all col-2">
+				<span> مشاهده همه </span>
+				<img src="@/assets/img/icon/arrow-left.svg" alt="" />
+			</div>
+		</div>
 		<div class="image-container">
 			<marketMediaCard
 				v-for="image in images"
@@ -24,7 +30,13 @@
 				:price="image.price"
 			/>
 		</div>
-		<span class="content-heading">صوت</span>
+		<div class="heading-container row">
+			<span class="content-heading col-10">صوت </span>
+			<div class="see-all col-2">
+				<span> مشاهده همه </span>
+				<img src="@/assets/img/icon/arrow-left.svg" alt="" />
+			</div>
+		</div>
 		<div class="video-container">
 			<marketMediaCard
 				v-for="item in audio"
@@ -35,7 +47,13 @@
 				:price="item.price"
 			/>
 		</div>
-		<span class="content-heading">ویدیو</span>
+		<div class="heading-container row">
+			<span class="content-heading col-10">ویدیو</span>
+			<div class="see-all col-2">
+				<span> مشاهده همه </span>
+				<img src="@/assets/img/icon/arrow-left.svg" alt="" />
+			</div>
+		</div>
 		<div class="audio-container">
 			<marketMediaCard
 				v-for="video in videos"
@@ -180,21 +198,39 @@ export default {
 
 <style scoped>
 .market-header {
-	margin-bottom: 40px;
+	margin-bottom: 48px;
 }
 .search-box {
 	width: 304px;
 	height: 32px;
 	border-radius: 21px !important;
 }
+.heading-container {
+	display: flex;
+	flex-direction: row;
+	width: 900px;
+}
 .content-heading {
 	position: relative;
 	bottom: 4px;
-	padding-right: 18px;
+	padding-right: 30px;
+	margin-top: 16px;
 	font-weight: 600;
 	font-size: 20px;
 	line-height: 23px;
 	color: #000000;
+}
+.see-all {
+	color: #4461f2;
+	text-align: right;
+	position: relative;
+	top: 16px;
+	right: 160px;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 23px;
+	cursor: pointer;
 }
 .audio-container,
 .video-container,
