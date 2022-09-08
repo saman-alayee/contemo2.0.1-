563e50245a30ce7b0259dcc0b516718c9818ae89
saman1945 <saman.alaii10@gmail.com>
2022-09-08 09:41:59 +0430

    topbars

--- src/layout/Topbar.vue
+++ src/layout/Topbar.vue
@@ -1,20 +1,22 @@
 <template>
 	<div class="topbar">
-		<div  v-if="$route.path !== `/Partners`" class="col-8">
+		<div v-if="$route.path !== `/Partners`" class="col-8">
 			<h3 class="">سلام وقت بخیر محسن هستم :)</h3>
 		</div>
 		<div v-if="$route.path == `/Partners`" class="col-8">
 			<h3 class="">متن مربوط به بخش همکاران</h3>
 		</div>
-		<div class="col-4"><wallet class="" /></div>
+		<div class="col-4" v-if="$route.path !== `/market`"><wallet class="" /></div>
+		<div class="col-3 app-ltr" v-if="$route.path == `/market`"><shoppingChip /></div>
 	</div>
 </template>
 
 <script>
 import wallet from '@/components/wallet.vue';
+import shoppingChip from '../components/elements/Chips/shoppingChip.vue';
 export default {
 	name: 'topbar',
-	components: { wallet },
+	components: { wallet, shoppingChip },
 };
 </script>
 
