e17ef4a86a514f8052098f544fa43b1fdd13c56e
saman1945 <saman.alaii10@gmail.com>
2022-09-10 14:17:55 +0430

    add-file

--- src/components/pageComponents/ContentCloud/ContentCloud.vue
+++ src/components/pageComponents/ContentCloud/ContentCloud.vue
@@ -2,23 +2,58 @@
 	<div class="dashboard-component">
 		<div class="row">
 			<pictureCloudCard v-for="content in pictures" :key="content.id" :file="content.file" />
+			<AddItem
+				class="mt-1"
+				width="25px"
+				height="25px"
+				borderHeight="111px"
+				borderWidth="104px"
+				border="2px dashed #4461F2"
+				text="بارگذاری فایل"
+			/>
 			<CloudCard
 				v-for="content in documents"
 				:key="content.id"
 				:name="content.name"
 				:file="content.file"
 			/>
+			<AddItem
+				class="mt-1"
+				width="25px"
+				height="25px"
+				borderHeight="111px"
+				borderWidth="104px"
+				border="2px dashed #4461F2"
+				text="بارگذاری فایل"
+			/>
 			<CloudCard
 				v-for="content in videos"
 				:key="content.id"
 				:name="content.name"
 				:file="content.file"
+			/><AddItem
+				class="mt-1"
+				width="25px"
+				height="25px"
+				borderHeight="111px"
+				borderWidth="104px"
+				border="2px dashed #4461F2"
+				text="بارگذاری فایل"
 			/>
 			<CloudCard
 				v-for="content in musics"
 				:key="content.id"
 				:name="content.name"
 				:file="content.file"
+			/>
+			<AddItem
+				class="mt-1"
+				width="25px"
+				height="25px"
+				borderHeight="111px"
+				borderWidth="104px"
+				border="2px dashed #4461F2"
+				text="بارگذاری فایل"
 			/>
 		</div>
 	</div>
@@ -27,15 +62,16 @@
 <script>
 import pictureCloudCard from '@/components/elements/Card/pictureCloudCard.vue';
 import CloudCard from '@/components/elements/Card/CloudCard.vue';
+import AddItem from '../../elements/AddItem/index.vue';
 export default {
 	components: {
 		pictureCloudCard,
 		CloudCard,
+		AddItem,
 	},
 	data() {
 		return {
 			pictures: [
-				{ file: 'vectors/modalPictures.svg' },
 				{ file: 'vectors/modalPictures.svg' },
 				{ file: 'vectors/modalPictures.svg' },
 				{ file: 'vectors/modalPictures.svg' },
@@ -54,7 +90,6 @@ export default {
 				{ file: 'vectors/documentContent.svg', name: 'نام فایل ' },
 				{ file: 'vectors/documentContent.svg', name: 'نام فایل ' },
 				{ file: 'vectors/documentContent.svg', name: 'نام فایل ' },
-				{ file: 'vectors/documentContent.svg', name: 'نام فایل ' },
 			],
 			videos: [
 				{ file: 'vectors/videoContent.svg', name: 'نام فایل ' },
@@ -65,10 +100,8 @@ export default {
 				{ file: 'vectors/videoContent.svg', name: 'نام فایل ' },
 				{ file: 'vectors/videoContent.svg', name: 'نام فایل ' },
 				{ file: 'vectors/videoContent.svg', name: 'نام فایل ' },
-				{ file: 'vectors/videoContent.svg', name: 'نام فایل ' },
 			],
 			musics: [
-				{ file: 'vectors/musicConetnt.svg', name: 'نام فایل ' },
 				{ file: 'vectors/musicConetnt.svg', name: 'نام فایل ' },
 				{ file: 'vectors/musicConetnt.svg', name: 'نام فایل ' },
 				{ file: 'vectors/musicConetnt.svg', name: 'نام فایل ' },
